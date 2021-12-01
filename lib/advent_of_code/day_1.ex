4b3f8938914c1fab1de2bb5254dc0b05fae17300
Bruno Paulino <bruno.paulino@n26.com>
2021-12-01 18:28:38 +0100

    Day 1 ✅

--- lib/advent_of_code/day_1.ex
+++ lib/advent_of_code/day_1.ex
@@ -1,7 +1,29 @@
 defmodule AdventOfCode.Day1 do
 
-  def detect_measurements(_depth_list) do
-    10
+  def detect_measurements() do
+    file_path = Path.join(:code.priv_dir(:adventofcode2021), "day_1_input.txt")
+    {:ok, content} = File.read(file_path)
+
+    {count, _} = content
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn str ->
+      {num, _} = Integer.parse(str)
+      num
+    end)
+    |> Enum.reduce({0, nil}, fn nextMeasurement, acc ->
+      case acc do
+        # No measurements before the first one
+        {0, nil} ->
+          {0, nextMeasurement}
+
+        # Only bump the count if there was an actual increase in the measurement
+        {count, lastMeasurement} ->
+          nextCount = if nextMeasurement > lastMeasurement, do: count + 1, else: count
+          {nextCount, nextMeasurement}
+      end
+    end)
+
+    count
   end
 
 end
--- test/advent_of_code/day_1_test.exs
+++ test/advent_of_code/day_1_test.exs
@@ -2,8 +2,6 @@ defmodule AdventOfCode.Day1Test do
   use ExUnit.Case
 
   test "detects how many measurements are larger than the previous measurement" do
-    file_path = Path.join(:code.priv_dir(:adventofcode2021), "day_1_input.txt")
-    {:ok, input} = File.read(file_path)
-    assert AdventOfCode.Day1.detect_measurements([]) === 10
+    assert AdventOfCode.Day1.detect_measurements() === 1665
   end
 end
